c4160dfae18bcc6b16135b12dd624bd175251ac0
Maria Gabriella Brodi <mbrodi@pivotal.io>
2021-08-24 20:02:51 -0400

    added a test for delition of non existing usernames

---
+++ springone-message-board-contracts/1.0.0/src/test/resources/contracts/message-board-admin/shouldFailDeleteMessageByUsername.groovy
@@ -0,0 +1,20 @@
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+    description("should fail delete the message by username")
+    request {
+        url("/message/anamethatdoesnotexist")
+        method(DELETE())
+        headers {
+            contentType(applicationJson())
+        }
+    }
+    response {
+        status(OK())
+        body([message: "Failure", type: "Delete", parameter: "-1"])
+        headers {
+            contentType(applicationJson())
+        }
+
+    }
+}
