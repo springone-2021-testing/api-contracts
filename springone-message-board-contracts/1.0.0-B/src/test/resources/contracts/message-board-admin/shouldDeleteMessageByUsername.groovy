56427916fa813fd8b4fee9b2dac58238558c3246
Cora Iberkleid <ciberkleid@gmail.com>
2021-08-26 19:24:37 -0400

    new files

---
+++ springone-message-board-contracts/1.0.0-B/src/test/resources/contracts/message-board-admin/shouldDeleteMessageByUsername.groovy
@@ -0,0 +1,20 @@
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+    description("should delete the message by username")
+    request {
+        url("/message/Cora")
+        method(DELETE())
+        headers {
+            contentType(applicationJson())
+        }
+    }
+    response {
+        status(OK())
+        body([message: "Success", type: "Delete", parameter: "1"])
+        headers {
+            contentType(applicationJson())
+        }
+
+    }
+}
