b6898fe8ec9e98686b21b4b5a4079a276e76af49
Maria Gabriella Brodi <mbrodi@pivotal.io>
2021-08-27 16:28:52 -0400

    Changing contract for admin to use the _ in the username

--- springone-message-board-contracts/1.0.0/src/test/resources/contracts/message-board-admin/shouldFailDeleteMessageByUsername.groovy
+++ springone-message-board-contracts/1.0.0/src/test/resources/contracts/message-board-admin/shouldFailDeleteMessageByUsername.groovy
@@ -3,7 +3,7 @@ import org.springframework.cloud.contract.spec.Contract
 Contract.make {
     description("should fail delete the message by username")
     request {
-        url("/message/anamethatdoesnotexist")
+        url("/message/anamethat_doesnotexist")
         method(DELETE())
         headers {
             contentType(applicationJson())
